9d996e5934b3e554690830a80c7d9a905f08f72d
MontainDew <hectormontagut29@gmail.com>
2026-03-02 10:35:56 +0100

    PlayerController
    CUANDOYOLAVIsupequeesamujereraparami

--- FirstPerson_RPMI/Assets/_VFX_Simple_Explosion/Assets/_FPS_RPMI_/Scripts/Player/FP_Controller.cs
+++ FirstPerson_RPMI/Assets/_VFX_Simple_Explosion/Assets/_FPS_RPMI_/Scripts/Player/FP_Controller.cs
@@ -12,12 +12,20 @@ public class FP_Controller : MonoBehaviour
     [SerializeField] float maxForce = 1f;
     [SerializeField] float sensitivity = 0.1f;
 
+    [Header("Jump and GroundCheck")]
+    [SerializeField] bool isGrounded;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0.3f;
+    [SerializeField] LayerMask groundLayer;
+
     [Header("Player State Bools")]
     [SerializeField] bool isSprinting;
     [SerializeField] bool isCrounching;
     #endregion
     //Variables de autoreferencia
     Rigidbody rb;
+    Animator anim;
 
     //Variables de input
     Vector2 moveInput;
@@ -27,6 +35,7 @@ public class FP_Controller : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
 
@@ -41,8 +50,51 @@ public class FP_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //GroundCheck
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
     }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void LateUpdate()
+    {
+        CameraLook();    
+    }
+
+    void CameraLook() 
+    {
+        //Rotacion del personaje (Horizaontal)
+        transform.Rotate(Vector3.up * lookInput.x * sensitivity);
+        //Rotacion del Camara (Vertical)
+        lookRotation += (-lookInput.y * sensitivity);
+        lookRotation = Mathf.Clamp(lookRotation, -90, 90);
+        camHolder.transform.localEulerAngles = new Vector3(lookRotation, 0f, 0f);
+    }
+    void Movement()
+    { 
+        //Definir los dos vectores que permiten la aceleración
+        Vector3 currentVelocity = rb.linearVelocity;
+        Vector3 targetVelocity = new Vector3(moveInput.x, 0, moveInput.y);
+        //A la dirección a alcanzar le multiplicamos la velocidad
+        targetVelocity *= isCrounching ? crouchSpeed : isSprinting ? sprintSpeed :speed;
+
+        //Convertir la direccion al eje mundial(world)
+        targetVelocity = transform.TransformDirection(targetVelocity);
+        //Calcular el cambio de velocidad(aceleración)
+        Vector3 velocityChange = (targetVelocity - currentVelocity);
+        velocityChange = new Vector3(velocityChange.x, 0, velocityChange.z);
+        velocityChange = Vector3.ClampMagnitude(velocityChange, maxForce);
+
+        //Aplicacion del movimiento(Direccion + Aceleración)
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+    void Jump()
+    {
+        if (isGrounded) rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
     #region Input Methods
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -56,17 +108,22 @@ public class FP_Controller : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-
+        if (context.performed) Jump();
     }
 
     public void OnCrouch(InputAction.CallbackContext context)
     {
-
+         if (context.performed) 
+        {
+            isCrounching = !isCrounching;
+            anim.SetBool("isCrouching", isCrounching);
+        }
     }
 
     public void OnSprint(InputAction.CallbackContext context)
     {
-
+        if (context.performed && !isCrounching) isSprinting = true;
+        if (context.canceled) isSprinting = false;
     }
     #endregion
 }
