119ddd725688e214c9ceb8fa7620a384dbb4b865
AdrianGalindoLlacer <adriangalindollacer@gmail.com>
2026-04-27 13:59:19 +0200

    Revert "otravezlomismo"
    This reverts commit cfecb5855bedaa541298758583a15a8a0ebbdc6a.

--- FirstPerson_RPMI/Assets/_Mansion_Root/Scripts/Enemy/EnemyAiBase.cs
+++ FirstPerson_RPMI/Assets/_Mansion_Root/Scripts/Enemy/EnemyAiBase.cs
@@ -177,18 +177,18 @@ public class EnemyAiBase : MonoBehaviour
     // ==========================================
     // PATRULLA (WAYPOINTS)
     // ==========================================
-   void PatrolLogic()
-{
-    agent.speed = patrolSpeed;
-
-    if (patrolPoints.Length == 0) return;
-
-    if (!agent.hasPath || HasReachedDestination())
+    void PatrolLogic()
     {
-        currentPatrolIndex = Random.Range(0, patrolPoints.Length);
-        agent.SetDestination(patrolPoints[currentPatrolIndex].position);
+        agent.speed = patrolSpeed;
+
+        if (patrolPoints.Length == 0) return;
+
+        if (!agent.hasPath || HasReachedDestination())
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+            agent.SetDestination(patrolPoints[currentPatrolIndex].position);
+        }
     }
-}
 
     // ==========================================
     // INVESTIGACIÓN
