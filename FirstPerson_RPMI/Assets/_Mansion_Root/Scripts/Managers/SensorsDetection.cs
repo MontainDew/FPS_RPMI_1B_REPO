a4f7a66570a07c44cb300482e04247615405a980
Karslos <cusodiaz1@gmail.com>
2026-04-27 14:47:40 +0200

    Revert "Merge branch 'dev' of https://github.com/MontainDew/FPS_RPMI_1B_REPO into dev"
    This reverts commit 0f7255adce4eccf6c9c218ba457e8846328f961e, reversing
    changes made to 0cdcbe93b7d2c45c861bb8e41816bccfb94cb1f5.

--- FirstPerson_RPMI/Assets/_Mansion_Root/Scripts/Managers/SensorsDetection.cs
+++ FirstPerson_RPMI/Assets/_Mansion_Root/Scripts/Managers/SensorsDetection.cs
@@ -39,6 +39,7 @@ public class SensorsDetection : MonoBehaviour
     {
         if (playerDetected == false && isDeactivated == true)
         {
+            Debug.Log("LISTO PARA DESCTIVAR");
             StartCoroutine(FlashedRoutine());
         }
     }
@@ -55,6 +56,8 @@ public class SensorsDetection : MonoBehaviour
 
     IEnumerator FlashedRoutine()
     {
+        Debug.Log("CAMBIO DE ESTADO");
+
         //Sonido desactivado
         detectionCollider.enabled = false;
         materialRenderer.material = deactivatedMat;
