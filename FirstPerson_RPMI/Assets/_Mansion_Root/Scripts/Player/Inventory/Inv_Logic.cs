3d8319fa9e5af7d852ce1b3a96cd335d3c7a4a67
Karslos <cusodiaz1@gmail.com>
2026-04-27 19:23:18 +0200

    Inventory Prefab

---
+++ FirstPerson_RPMI/Assets/_Mansion_Root/Scripts/Player/Inventory/Inv_Logic.cs
@@ -0,0 +1,107 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Inv_Logic : MonoBehaviour
+{
+    #region Variables
+    [Header("Checker")]
+    [SerializeField] private bool Camera;
+    [SerializeField] private int FuseCuantity;
+    [SerializeField] private bool Key;
+    [SerializeField] private bool HColorCode;
+    [SerializeField] private bool ColorCode;
+    [SerializeField] private bool DoorCode;
+
+    [Header("ItemSlots")]
+    [SerializeField] private GameObject CameraSlot;
+    [SerializeField] private GameObject FuseSlot;
+    [SerializeField] private TMP_Text FuseCuantityText;
+    [SerializeField] private GameObject KeySlot;
+    [SerializeField] private GameObject HColorCodeSlot;
+    [SerializeField] private GameObject ColorCodeSlot;
+    [SerializeField] private GameObject DoorCodeSlot;
+
+    [Header("Other")]
+    [SerializeField] private GameObject Inventory;
+    [SerializeField] private int onoff = 1;
+    #endregion
+
+    private void Update()
+    {
+        if (FuseCuantity > 0)
+        {
+            FuseSlot.SetActive(true);
+            FuseCuantityText.text = "X " + FuseCuantity.ToString();
+        }
+        else
+        {
+            FuseSlot.SetActive(false);
+        }
+        if (HColorCode)
+        {
+            HColorCodeSlot.SetActive(true);
+        }
+        else
+        {
+            HColorCodeSlot.SetActive(false);
+        }
+        if (DoorCode)
+        {
+            DoorCodeSlot.SetActive(true);
+        }
+        else
+        {
+            DoorCodeSlot.SetActive(false);
+        }
+        if (Camera)
+        {
+            CameraSlot.SetActive(true);
+        }
+        else
+        {
+            CameraSlot.SetActive(false);
+        }
+        if (Key)
+        {
+            KeySlot.SetActive(true);
+        }
+        else
+        {
+            KeySlot.SetActive(false);
+        }
+        if (ColorCode)
+        {
+            ColorCodeSlot.SetActive(true);
+        }
+        else
+        {
+            ColorCodeSlot.SetActive(false);
+        }
+    }
+    #region inventory
+    public void OnInventory(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (onoff == 1)
+            {
+                Debug.Log("Abre");
+                Inventory.SetActive(true);
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+                onoff++;
+            }
+            else
+            {
+                Debug.Log("Cierra");
+                Inventory.SetActive(false);
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                onoff--;
+            }
+        }
+    }
+}
+    #endregion
