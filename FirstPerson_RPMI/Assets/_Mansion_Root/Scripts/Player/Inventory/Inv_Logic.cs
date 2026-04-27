104c651655e3c5e6c9afe39909187fdd827f4d29
Karslos <cusodiaz1@gmail.com>
2026-04-27 14:48:24 +0200

    A dios le pido

--- FirstPerson_RPMI/Assets/_Mansion_Root/Scripts/Player/Inventory/Inv_Logic.cs
+++ FirstPerson_RPMI/Assets/_Mansion_Root/Scripts/Player/Inventory/Inv_Logic.cs
@@ -1,60 +1,10 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Inv_Logic : MonoBehaviour
 {
-    [Header("Checker")]
-    [SerializeField] private bool Camera;
-    [SerializeField] private int FuseCuantity;
-    [SerializeField] private bool HColorCode;
-    [SerializeField] private bool ColorCode;
-    [SerializeField] private bool DoorCode;
-
-    [Header("ItemSlots")]
-    [SerializeField] private GameObject CameraSlot;
-    [SerializeField] private GameObject FuseSlot;
-    [SerializeField] private TMP_Text FuseCuantityText;
-    [SerializeField] private GameObject HColorCodeSlot;
-    [SerializeField] private GameObject ColorCodeSlot;
-    [SerializeField] private GameObject DoorCodeSlot;
-
-    [Header("Other")]
-    [SerializeField]private GameObject Inventory;
+    [SerializeField] private GameObject Inventory;
     [SerializeField]private int onoff = 1;
-
-    private void Update()
-    {
-        if (FuseCuantity > 0)
-        {
-            FuseSlot.SetActive(true);
-            FuseCuantityText.text = "X " + FuseCuantity.ToString();
-        }
-        else
-        {
-            FuseSlot.SetActive(false);
-        }
-        if (HColorCode)
-        {
-            HColorCodeSlot.SetActive(true);
-        }
-        else
-        {
-            HColorCodeSlot.SetActive(false);
-        }
-        if (DoorCode)
-        {
-            DoorCodeSlot.SetActive(true);
-        }
-        if (Camera)
-        {
-            CameraSlot.SetActive(true);
-        }
-        if (ColorCode)
-        {
-            ColorCodeSlot.SetActive(true);
-        }
-    }
     #region inventory
     public void OnInventory(InputAction.CallbackContext context)
     {
